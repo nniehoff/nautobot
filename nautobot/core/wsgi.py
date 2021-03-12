5a4d018754844d569930f8d12c52b7f996ec0b4d
nniehoff <github@nickniehoff.net>
2021-03-12 14:37:34 -0700

    DB SSL Fix

--- nautobot/core/wsgi.py
+++ nautobot/core/wsgi.py
@@ -1,5 +1,5 @@
 from django.core.wsgi import get_wsgi_application
-
+from django.db import connection
 import nautobot
 
 # This is the Django default left here for visibility on how the Nautobot pattern
@@ -9,5 +9,8 @@ import nautobot
 # Instead of importing `DJANGO_SETTINGS_MODULE` we're using the custom loader
 # pattern from `nautobot.core.runner` to read environment or config path for us.
 nautobot.setup()
+
+# Close existing connections to the DB before the application is forked
+connection.close()
 
 application = get_wsgi_application()
